0bdf1d915e266651660cd5f70dccc1c73140ac0e
AVIRAL BHATNAGAR <bhatnagaraviral1@gmail.com>
2026-07-04 19:36:39 +0530

    feat: implement logout functionality and update navbar visibility based on authentication state

---
+++ JS/navbar.script.js
@@ -0,0 +1,36 @@
+const logOutbutton = document.getElementById("Logout")
+const applicationlink = document.getElementById("applicationlink")
+const applicationslink = document.getElementById("applicationslink")
+const Loginlink = document.getElementById("Loginlink")
+const registerlink = document.getElementById("registerlink")
+
+
+console.log(logOutbutton);
+const token = localStorage.getItem("accesstoken")
+ if(token){
+        Loginlink.style.display = "none";
+        registerlink.style.display = "none";
+    }
+    else{
+        logOutbutton.style.display = "none";
+    }
+
+
+logOutbutton.addEventListener("click",async function (e){
+    e.preventDefault()
+    const response = await fetch("https://internship-application-tracker-dind.onrender.com/api/v1/application/logout",{
+        method:"POST",
+        headers:{
+            "Authorization": `Bearer ${token}`,
+            "Content-Type": "application/json"
+        }
+    })
+    if(!response.ok){
+        return false
+    }
+    const data = await response.json()
+    console.log(data)
+
+    localStorage.removeItem("accesstoken");
+    window.location.replace("Login.html");
+})
