7be877115000e7f737f692dc3d3c18b2e5981e3d
AVIRAL BHATNAGAR <bhatnagaraviral1@gmail.com>
2026-07-04 19:36:30 +0530

    feat: add logout route and update navigation links in the application

--- backend/router/application.router.js
+++ backend/router/application.router.js
@@ -4,16 +4,19 @@ import { applications } from "../controllers/applications.controller.js";
 import { deleteApplication } from "../controllers/delete.controller.js";
 import { applicationsByuid } from "../controllers/applications.controller.js";
 import { updateApplication } from "../controllers/update.controller.js";
-import { loginUser, registerUser } from "../controllers/user.controller.js";
+import { loginUser, logout, registerUser } from "../controllers/user.controller.js";
+import { verifyJWT } from "../middleware/auth.middleware.js";
 
 const router = Router()
 router.route("/registerUser").post(registerUser)
 
 router.route("/loginUser").post(loginUser)
 
-router.route("/applicationform").post(applicationform)
+router.route("/logout").post(verifyJWT ,logout)
 
-router.route("/applications").get(applications)
+router.route("/applicationform").post(verifyJWT ,applicationform)
+
+router.route("/applications").get(verifyJWT ,applications)
 router.route("/applicationsByuid/:id").get(applicationsByuid)
 router.route("/deleteApplication/:id").delete(deleteApplication)
 
